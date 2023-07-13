86ea076734d72ddaa3c205b667de245f1bcd5720
Dickson <zerorich1225@gmail.com>
2023-07-13 19:14:45 +0800

    7/13

--- src/main/java/tw/gymlife/com/dao/impl/ComFrontUtilImpl.java
+++ src/main/java/tw/gymlife/com/dao/impl/ComFrontUtilImpl.java
@@ -35,7 +35,7 @@ import tw.gymlife.com.model.OrdersDetailsDTO;
 @Repository
 public class ComFrontUtilImpl implements ComFrontUtil {
 	
-	private String pathAll="C:\\testGym\\gymproject\\src\\main\\resources\\static\\gym\\com\\cart";
+	private String pathAll="C:\\GYMLIFEProject\\GYMLIFE\\src\\main\\resources\\static\\gym\\com\\cart";
 	private String path = "C:\\springBoot\\workspace\\springBootGym\\src\\main\\resources\\static\\gym\\com\\cart";
 
 	// 將Bean轉成DTO

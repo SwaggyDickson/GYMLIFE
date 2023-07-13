11674db7959a9431fafbe58782716d6311343210
Huang <h96716@gmail.com>
2023-07-13 20:08:08 +0800

    07/13

--- src/main/java/tw/gymlife/activity/controller/ActivityFrontController.java
+++ src/main/java/tw/gymlife/activity/controller/ActivityFrontController.java
@@ -4,7 +4,8 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Date;
 import java.util.List;
-
+import org.springframework.ui.Model;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import tw.gymlife.activity.model.Registration;
 
 import tw.gymlife.activity.model.Activity;
+import tw.gymlife.activity.model.NominatimResult;
 import tw.gymlife.activity.service.ActivityService;
 import tw.gymlife.activity.service.RegistrationService;
 
@@ -47,6 +49,10 @@ public class ActivityFrontController {
 	    // 根據活動ID找到該筆資料
 	    Activity activity = aService.getActivityById(activityId);
 	    
+	    // 获取活动地点
+	    String activityLocation = activity.getActivityLocation();
+
+	    
 	    // 獲取活動的報名截止日期和當前日期進行比較
 	    Date registrationEndDate = activity.getRegistrationEndDate();
 	    Date currentDate = new Date();
@@ -62,6 +68,8 @@ public class ActivityFrontController {
 	    
 	    // 利用model傳給前端
 	    m.addAttribute("activity", activity);
+	    // 将活动地点添加到模型中
+	    m.addAttribute("activityLocation", activityLocation);
 	    
         // 計算倒計時時間差 報名截止日 & 活動日期倒數計時器
         Date now = new Date();
@@ -78,24 +86,35 @@ public class ActivityFrontController {
 	    return "frontgymlife/activity/Details";
 	}
 	
-//	@GetMapping("/map")
-//	public String showMapPage(@RequestParam("activityId") Integer activityId, Model m) {
-//	    // 根据活动ID找到该笔数据
-//	    Activity activity = aService.getActivityById(activityId);
-//	    
-//	    // 获取活动地点
-//	    String activityLocation = activity.getActivityLocation();
-//
-//	    // 使用地理编码服务将地址转换为经纬度
-//	    double latitude = // 调用逆地理编码服务将地址转换为经度
-//	    double longitude = // 调用逆地理编码服务将地址转换为纬度
-//
-//	    // 将经纬度添加到模型对象
-//	    model.addAttribute("latitude", latitude);
-//	    model.addAttribute("longitude", longitude);
-//
-//	    return "map"; // 返回前端页面的文件名，例如 "map.html" 或 "map"（如果使用 Thymeleaf）
-//	}
+	// AJAX API：获取活动地点的经纬度
+    @GetMapping("/getActivityLocation")
+    public String getActivityLocation(@RequestParam("activityId") Integer activityId, Model m) {
+        // 根据活动ID找到该笔数据
+        Activity activity = aService.getActivityById(activityId);
 
+        // 获取活动地点
+        String activityLocation = activity.getActivityLocation();
 
+        // 使用Nominatim进行Geocoding，获取活动地点的经纬度
+        String nominatimUrl = "https://nominatim.openstreetmap.org/search?format=json&q=";
+        RestTemplate restTemplate = new RestTemplate();
+        String url = nominatimUrl + activityLocation;
+        NominatimResult[] results = restTemplate.getForObject(url, NominatimResult[].class);
+
+        if (results != null && results.length > 0) {
+            // 获取第一个结果的经纬度
+            double activityLatitude = results[0].getLat();
+            double activityLongitude = results[0].getLon();
+
+            // 将活动地点的经纬度添加到Model对象中
+            m.addAttribute("activityLatitude", activityLatitude);
+            m.addAttribute("activityLongitude", activityLongitude);
+        } else {
+            // 处理找不到结果的情况
+            // ...
+        }
+
+        return "activityLocation";
+    }
 }
+

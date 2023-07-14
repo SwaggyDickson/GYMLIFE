4bc3c70f3ba796bbfcd42bdf4fc36e15789924be
SWAGGY\zeror <zerorich1225@gmail.com>
2023-07-15 04:24:06 +0800

    Course 7/15 part 2

--- src/main/resources/static/gym/course/back/courseAnalyze.js
+++ src/main/resources/static/gym/course/back/courseAnalyze.js
@@ -1,7 +1,22 @@
 //訂單圖表生成
 $(document).ready(function() {
+	viewAnalyze();
+//	buyerAnalyze();
+	});
+	$('#viewbtn').on('click',function(){
+		viewAnalyze();
+	})
+	$('#buybtn').on('click',function(){
+		buyerAnalyze();
+	})
+	$('#buytimebtn').on('click',function(){
+		buyTimeAnalyze();
+	})
+	
+	//觀看人數圖表
+	function viewAnalyze(){
 	let corderAnalyze = document.getElementById('corderAnalyze');
-	corderAnalyze.innerHTML = `<canvas id="courseView"></canvas><canvas id="courseBuyers"></canvas>`;
+	corderAnalyze.innerHTML = `<canvas id="courseView"></canvas>`;
 	axios({
 		method: 'get',
 		url: 'http://localhost:8080/gymlife/course/corder/analyze'
@@ -62,6 +77,18 @@ $(document).ready(function() {
 					}
 				}
 			});
+
+		})
+};
+	//購買人數圖表
+	function buyerAnalyze(){
+	let corderAnalyze = document.getElementById('corderAnalyze');
+	corderAnalyze.innerHTML = `<canvas id="courseBuyers"></canvas>`;
+	axios({
+		method: 'get',
+		url: 'http://localhost:8080/gymlife/course/corder/analyze'
+	})
+		.then(res => {
 			const Buyers = document.getElementById('courseBuyers');
 			let BuyerscourseName = [];
 			let Buyersdata = [];
@@ -118,7 +145,93 @@ $(document).ready(function() {
 			});
 
 		})
-});
+};
+	//購買時間圖表
+	function getMonthLabels(count) {
+  const currentDate = new Date();
+  const labels = [];
+
+  for (let i = 0; i < count; i++) {
+    const month = currentDate.getMonth();
+    const year = currentDate.getFullYear();
+    const label = `${year}-${month + 1}`;
+    labels.push(label);
+    currentDate.setMonth(month - 1);
+  }
+
+  return labels.reverse();
+}
+
+//const labels = getMonthLabels(7);
+//console.log(labels);
+	function buyTimeAnalyze(){
+	let corderAnalyze = document.getElementById('corderAnalyze');
+	corderAnalyze.innerHTML = `<canvas id="courseBuyTime"></canvas>`;
+//	axios({
+//		method: 'get',
+//		url: 'http://localhost:8080/gymlife/course/corder/analyze'
+//	})
+//		.then(res => {
+			const BuyTime = document.getElementById('courseBuyTime');
+//			let BuyerscourseName = [];
+//			let Buyersdata = [];
+//			let Buyerscolors = [];
+//			for (i = 0; i < res.data.length; i++) {
+//				BuyerscourseName.push(res.data[i].courseName)
+//				Buyersdata.push(res.data[i].courseBuyers);
+//				//根據資料長度生成不同顏色
+//				let Buyerscolor = generateColor();
+//				Buyerscolors.push(Buyerscolor);
+//			}
+//			console.log(Buyersdata)
+//			console.log(Buyerscolors)
+const labels = getMonthLabels(7);
+			new Chart(BuyTime, {
+				type: 'line',
+				data: {
+					 labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+				},
+//				options: {
+////					scales: {
+////						y: {
+////							beginAtZero: true
+////						}
+////					},
+//					animation: {
+//						//						animateRotate:  false,
+//						animateScale: true
+//
+//					}, plugins: {
+//						title: {
+//							display: true,
+//							text: '購買人數',
+//							font: {
+//                        size: 20
+//                    }
+//						},legend:{
+//							position:'bottom',font: {
+//                       
+//                    },labels: {
+//                    // This more specific font property overrides the global property
+//                    font: {
+//                        size: 18
+//                    }
+//                }
+//						}
+//					}
+//				}
+			});
+
+//		}
+//		)
+};
 
 // 生成顏色的函數
 function generateColor() {
--- src/main/resources/static/gym/course/front/coursebuy.js
+++ src/main/resources/static/gym/course/front/coursebuy.js
@@ -3,79 +3,27 @@ $('#buybtn').on('click', function(e) {
 	$('#buycourseId').click();
 })
 //課程價格計算
-$('#buynumber').on('change', function(e) {
-	let courseCost = $('#courseCost').val();
-	let buynumber = $('#buynumber').val();
-	//  console.log(buynumber);
-	$('#allcost').text(buynumber * courseCost);
-	$('#allcost').text(buynumber * courseCost);
-	$('#corderCost').val(buynumber * courseCost);
+function calculateCost() {
+  let courseCost = $('#courseCost').val();
+  let buynumber = $('#buynumber').val();
+
+  $('#allcost').text(buynumber * courseCost);
+  $('#corderCost').val(buynumber * courseCost);
 	console.log($('#corderCost').val())
+}
+$('#buynumber').on('change', function(e) {
+	calculateCost();
 });
-//$('#paybtn').on('click', function(e) {
-//	e.preventDefault();
-//	let paymodal = document.getElementById('paymodal');
-//	let userName = document.getElementById('userName').value;
-//	let allcost = $('#allcost').text();
-//	let html = '';
-//	html += `<hr><div class="row">
-//						<div class="col-lg-12">
-//							<span>購買人:</span>${userName}
-//						</div>
-//						<div class="col-lg-12">
-//							<a>總金額:</a><a id="payallcost">${allcost}</a>
-//						</div>
-//						<div class="col-lg-12">
-//							<a>付款方式:</a>
-//						</div>
-//						<div class="col-lg-12">
-//							<button type="button" class="btn btn-danger" onclick="insertcorder()">確認付款</button>
-//						</div>
-//					</div>`;
-//	paymodal.innerHTML = html;
-//})
-//新增訂單
-//function insertcorder() {
-//	let userId = document.getElementById('userId').value;
-//	let courseId = document.getElementById('courseId').value;
-//	let corderQuantity = document.getElementById('buynumber').value;
-//	let corderCost = document.getElementById('allcost').text;
-//	console.log(userId);
-//	console.log(courseId);
-//	console.log(corderQuantity);
-//	console.log(corderCost);
-//	Swal.fire({
-//		title: '確定要購買嗎?',
-//		showDenyButton: true,
-//		confirmButtonText: '確定',
-//		denyButtonText: `取消`,
-//	}).then((result) => {
-//		if (result.isConfirmed) {
-//			axios({
-//				method: 'post',
-//				url: 'http://localhost:8080/gymlife/course/order/insert',
-//				params: {
-//					'userId': userId,
-//					'courseId': courseId,
-//					'corderPayment': '完成',
-//					'corderQuantity': corderQuantity,
-//					'corderCost': corderCost
-//				}
-//			}).then(res => {
-//				axios.post('http://localhost:8080/gymlife/ecpayCheckout')
-//				//跳轉回教練課程
-////				console.log('res' + JSON.stringify(res));
-////				window.location.href = 'http://localhost:8080/gymlife/ecpayCheckout';
-////				Swal.fire(
-////					'購買成功!',
-////					'',
-////					'success'
-////				)
-//
-//			})
-//		} else if (result.isDenied) {
-//		}
-//	})
-//}
-
-
+//購買堂數按鈕
+$('#input-plus').on('click', function() {
+  let buynumber = $('#buynumber');
+  buynumber.val(parseInt(buynumber.val()) + 1);
+  calculateCost();
+});
+$('#input-minus').on('click', function() {
+	let buynumber = $('#buynumber');
+  if (parseInt(buynumber.val()) > 1) {
+    buynumber.val(parseInt(buynumber.val())-1);
+    calculateCost();
+  }
+});

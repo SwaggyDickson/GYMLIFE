e9c4397f754e2fd881bf05e2a95840ac64f52923
SWAGGY\zeror <zerorich1225@gmail.com>
2023-07-16 21:47:47 +0800

    Course 7/16 part 1

--- src/main/resources/static/gym/course/back/courseAnalyze.js
+++ src/main/resources/static/gym/course/back/courseAnalyze.js
@@ -1,22 +1,15 @@
 //訂單圖表生成
 $(document).ready(function() {
+	let corderAnalyze = document.getElementById('corderAnalyze');
+	corderAnalyze.innerHTML = `<canvas id="courseView"></canvas><canvas id="courseBuyers"></canvas>`;
 	viewAnalyze();
-//	buyerAnalyze();
+	buyerAnalyze();
 	});
-	$('#viewbtn').on('click',function(){
-		viewAnalyze();
-	})
-	$('#buybtn').on('click',function(){
-		buyerAnalyze();
-	})
-	$('#buytimebtn').on('click',function(){
-		buyTimeAnalyze();
-	})
 	
 	//觀看人數圖表
 	function viewAnalyze(){
-	let corderAnalyze = document.getElementById('corderAnalyze');
-	corderAnalyze.innerHTML = `<canvas id="courseView"></canvas>`;
+//	let corderAnalyze = document.getElementById('corderAnalyze');
+//	corderAnalyze.innerHTML = `<canvas id="courseView"></canvas>`;
 	axios({
 		method: 'get',
 		url: 'http://localhost:8080/gymlife/course/corder/analyze'
@@ -35,7 +28,7 @@ $(document).ready(function() {
 			}
 			console.log(data)
 			console.log(colors)
-			new Chart(view, {
+			const ViewChat = new Chart(view, {
 				type: 'doughnut',
 				data: {
 					labels: viewName,
@@ -77,13 +70,21 @@ $(document).ready(function() {
 					}
 				}
 			});
+	const viewbtn = document.getElementById('viewbtn');
+            viewbtn.addEventListener('click', () => {
+                const image = ViewChat.toBase64Image();
+                const link = document.createElement('a');
+                link.href = image;
+                link.download = 'Viewchart.png';
+                link.click();
+            });
 
 		})
 };
 	//購買人數圖表
 	function buyerAnalyze(){
-	let corderAnalyze = document.getElementById('corderAnalyze');
-	corderAnalyze.innerHTML = `<canvas id="courseBuyers"></canvas>`;
+//	let corderAnalyze = document.getElementById('corderAnalyze');
+//	corderAnalyze.innerHTML = `<canvas id="courseBuyers"></canvas>`;
 	axios({
 		method: 'get',
 		url: 'http://localhost:8080/gymlife/course/corder/analyze'
@@ -102,7 +103,7 @@ $(document).ready(function() {
 			}
 			console.log(Buyersdata)
 			console.log(Buyerscolors)
-			new Chart(Buyers, {
+			const buyChat = new Chart(Buyers, {
 				type: 'doughnut',
 				data: {
 					labels: BuyerscourseName,
@@ -143,95 +144,20 @@ $(document).ready(function() {
 					}
 				}
 			});
+			const buybtn = document.getElementById('buybtn');
+            buybtn.addEventListener('click', () => {
+                const image = buyChat.toBase64Image();
+                const link = document.createElement('a');
+                link.href = image;
+                link.download = 'Buyerchart.png';
+                link.click();
+            });
+
 
 		})
+		
 };
-	//購買時間圖表
-	function getMonthLabels(count) {
-  const currentDate = new Date();
-  const labels = [];
 
-  for (let i = 0; i < count; i++) {
-    const month = currentDate.getMonth();
-    const year = currentDate.getFullYear();
-    const label = `${year}-${month + 1}`;
-    labels.push(label);
-    currentDate.setMonth(month - 1);
-  }
-
-  return labels.reverse();
-}
-
-//const labels = getMonthLabels(7);
-//console.log(labels);
-	function buyTimeAnalyze(){
-	let corderAnalyze = document.getElementById('corderAnalyze');
-	corderAnalyze.innerHTML = `<canvas id="courseBuyTime"></canvas>`;
-//	axios({
-//		method: 'get',
-//		url: 'http://localhost:8080/gymlife/course/corder/analyze'
-//	})
-//		.then(res => {
-			const BuyTime = document.getElementById('courseBuyTime');
-//			let BuyerscourseName = [];
-//			let Buyersdata = [];
-//			let Buyerscolors = [];
-//			for (i = 0; i < res.data.length; i++) {
-//				BuyerscourseName.push(res.data[i].courseName)
-//				Buyersdata.push(res.data[i].courseBuyers);
-//				//根據資料長度生成不同顏色
-//				let Buyerscolor = generateColor();
-//				Buyerscolors.push(Buyerscolor);
-//			}
-//			console.log(Buyersdata)
-//			console.log(Buyerscolors)
-const labels = getMonthLabels(7);
-			new Chart(BuyTime, {
-				type: 'line',
-				data: {
-					 labels: labels,
-  datasets: [{
-    label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
-    fill: false,
-    borderColor: 'rgb(75, 192, 192)',
-    tension: 0.1
-  }]
-				},
-//				options: {
-////					scales: {
-////						y: {
-////							beginAtZero: true
-////						}
-////					},
-//					animation: {
-//						//						animateRotate:  false,
-//						animateScale: true
-//
-//					}, plugins: {
-//						title: {
-//							display: true,
-//							text: '購買人數',
-//							font: {
-//                        size: 20
-//                    }
-//						},legend:{
-//							position:'bottom',font: {
-//                       
-//                    },labels: {
-//                    // This more specific font property overrides the global property
-//                    font: {
-//                        size: 18
-//                    }
-//                }
-//						}
-//					}
-//				}
-			});
-
-//		}
-//		)
-};
 
 // 生成顏色的函數
 function generateColor() {

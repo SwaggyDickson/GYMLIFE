3bf5809a5b594cf4958f031ceeb8f224c3d8072e
Jack <Jack@DESKTOP-P3EMR16>
2023-09-08 17:13:26 +0800

    CSS錯誤更改

---
+++ src/main/java/tw/gymlife/course/model/Practice.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tw.gymlife.course.model;
+
+/**
+ *
+ * @author Jack
+ */
+public class Practice {
+    private String id;
+    private String name;
+    private String phonenumber;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the phonenumber
+     */
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    /**
+     * @param phonenumber the phonenumber to set
+     */
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+    
+}
